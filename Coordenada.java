1f6d07efbef646e213fb5591641bc99057dc9a4e
henry <henrycanalesv@gmail.com>
2022-04-26 22:32:41 -0500

    Tablero grafico

---
+++ Coordenada.java
@@ -0,0 +1,11 @@
+package IniciativaAjedrez;
+
+public class Coordenada {
+	public final int X;
+	public final int Y;
+	
+	public Coordenada(int X, int Y) {
+		this.X = X;
+		this.Y = Y;
+	}
+}
---
+++ Pieza.java
@@ -0,0 +1,7 @@
+package IniciativaAjedrez;
+
+public abstract class Pieza {
+	Coordenada[] posiblesMovimientos;
+	
+	abstract boolean checarMovimiento(Coordenada coordenada);
+}
